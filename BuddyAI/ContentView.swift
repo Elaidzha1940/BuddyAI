5a51c2b1df5221dbf9f927ccf72a6234fe7e1db4
Elaidzha1940 <eli080293@gmail.com>
2023-09-01 13:57:23 +0300

    BuddyAI + brain

--- BuddyAI/ContentView.swift
+++ BuddyAI/ContentView.swift
@@ -14,24 +14,41 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack {
-            ZStack {
-                Image("setka")
-                    .offset(x: 120, y: -160)
-                
-                Button {
-                    //action
-                } label: {
-                    Text("AI Buddy")
+        VStack(spacing: 1) {
+            VStack {
+                ZStack {
+                    Image("setka")
+                        .offset(x: 120, y: -160)
+                    
+                    Button {
+                        //action
+                    } label: {
+                        Text("AI Buddy")
+                    }
+                    .font(.system(size: 20, weight: .bold, design: .rounded))
+                    .foregroundColor(.black)
+                    .frame(width: 110, height: 40)
+                    .background(Color.white)
+                    .cornerRadius(20)
                 }
-                .font(.system(size: 20, weight: .bold, design: .rounded))
-                .foregroundColor(.black)
-                .frame(width: 110, height: 40)
-                .background(Color.white)
-                .cornerRadius(20)
                 
+                HStack {
+                    Circle()
+                        .frame(width: 6, height: 6)
+                    Text("Online")
+                        .font(.system(size: 15, weight: .bold, design: .rounded))
+                        .foregroundColor(.secondary)
+                    
+                }
+                .padding(.bottom, 150)
+                                
+                Image("brain")
+                
+                Spacer()
+                
+                Text("")
+
             }
-            Spacer()
         }
         .preferredColorScheme(.dark)
     }
